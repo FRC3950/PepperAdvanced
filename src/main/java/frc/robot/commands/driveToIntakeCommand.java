e3e8fb205b9eb9a4ae5be6926990f0c430f04197
CJ Brandi <cj.brandi10@gmail.com>
2025-09-16 11:39:28 -0400

    redo auto source?

--- src/main/java/frc/robot/commands/driveToIntakeCommand.java
+++ src/main/java/frc/robot/commands/driveToIntakeCommand.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import com.pathplanner.lib.auto.AutoBuilder;
@@ -18,85 +14,70 @@ import frc.robot.subsystems.LightsSubsystem;
 import frc.robot.subsystems.drive.Drive;
 import java.util.function.DoubleSupplier;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class driveToIntakeCommand extends Command {
-  private final LightsSubsystem lights;
-  private final Drive drive;
-  private final DoubleSupplier driveStickMoved;
-  private Pose2d targetPose;
-  private Command pathCommand;
-  private Pose2d targetPoseToUseInAutoNavigate;
+    private final LightsSubsystem lights;
+    private final Drive drive;
+    private final DoubleSupplier driveStickMoved;
+    private final PathConstraints constraints = new PathConstraints(3.0, 3.0, Units.degreesToRadians(540), Units.degreesToRadians(540));
+    private static final AprilTagFieldLayout aprilTagLayoutForAutoDrive =
+        AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+    private final int[] aprilTagIdsForScoring = new int[] {1, 2, 12, 13};
+    private final Pose2d[] poseForScoringIDs;
+    private Command pathCommand;
 
-  public PathConstraints constraints =
-      new PathConstraints(3.0, 3.0, Units.degreesToRadians(540), Units.degreesToRadians(540));
-
-  // AprilTag layout
-  public static AprilTagFieldLayout aprilTagLayoutForAutoDrive =
-      AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
-
-  int[] aprilTagIdsForScoring = new int[] {1, 2, 12, 13};
-  Pose2d[] poseForScoringIDs;
-
-  /** Creates a new driveToScoreCommand. */
-  public driveToIntakeCommand(Drive drive, LightsSubsystem lights, DoubleSupplier driveStickMoved) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.lights = lights;
-    this.drive = drive;
-    this.driveStickMoved = driveStickMoved;
-    addRequirements(drive, lights);
-
-    poseForScoringIDs = new Pose2d[aprilTagIdsForScoring.length];
-
-    for (int i = 0; i < aprilTagIdsForScoring.length; i++) {
-      poseForScoringIDs[i] =
-          aprilTagLayoutForAutoDrive
-              .getTagPose(aprilTagIdsForScoring[i])
-              .orElse(new Pose3d())
-              .toPose2d();
-    }
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    // lights.setLEDOverride(true, AnimationType.Strobe);
-    // for each pose in poseForScoringIDs, find the closest one to the current pose
-    Pose2d currentPose = drive.getPose();
-    double minDistance = 1000.0;
-    Pose2d closestPose = new Pose2d();
-
-    for (Pose2d pose : poseForScoringIDs) {
-
-      double distance = pose.getTranslation().getDistance(currentPose.getTranslation());
-      if (distance < minDistance) {
-        minDistance = distance;
-        closestPose = pose;
-      }
+    public driveToIntakeCommand(Drive drive, LightsSubsystem lights, DoubleSupplier driveStickMoved) {
+        this.lights = lights;
+        this.drive = drive;
+        this.driveStickMoved = driveStickMoved;
+        addRequirements(drive, lights);
+        poseForScoringIDs = new Pose2d[aprilTagIdsForScoring.length];
+        for (int i = 0; i < aprilTagIdsForScoring.length; i++) {
+            poseForScoringIDs[i] = aprilTagLayoutForAutoDrive
+                .getTagPose(aprilTagIdsForScoring[i])
+                .orElse(new Pose3d())
+                .toPose2d();
+        }
     }
 
-    targetPose = closestPose;
+    @Override
+    public void initialize() {
+        // For each pose, find the closest one to the current pose
+        Pose2d currentPose = drive.getPose();
+        double minDistance = 1000.0;
+        Pose2d closestPose = new Pose2d();
+        for (Pose2d pose : poseForScoringIDs) {
+            double distance = pose.getTranslation().getDistance(currentPose.getTranslation());
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestPose = pose;
+            }
+        }
+        Pose2d targetPose = closestPose.transformBy(new Transform2d(.5, .0, new Rotation2d(0)));
+        // Compose the path following command (do NOT schedule!)
+        pathCommand = AutoBuilder.pathfindToPose(targetPose, constraints, 0.0);
+        pathCommand.initialize();
+        // Optionally: lights.setLEDOverride(true, AnimationType.Strobe);
+    }
 
-    targetPose = targetPose.transformBy(new Transform2d(.5, .0, new Rotation2d(0)));
+    @Override
+    public void execute() {
+        if (pathCommand != null) {
+            pathCommand.execute();
+        }
+    }
 
-    pathCommand = AutoBuilder.pathfindToPose(targetPose, constraints, 0.0);
-    pathCommand.schedule();
-  }
+    @Override
+    public void end(boolean interrupted) {
+        if (pathCommand != null) {
+            pathCommand.end(interrupted);
+        }
+        // Optionally: lights.setLEDOverride(false, null);
+    }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    // lights.setLEDOverride(false, null);
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return pathCommand == null
-        || pathCommand.isFinished()
-        || Math.abs(driveStickMoved.getAsDouble()) > 0.5; // Stop when PathPlanner finishes
-  }
+    @Override
+    public boolean isFinished() {
+        return pathCommand == null
+            || pathCommand.isFinished()
+            || Math.abs(driveStickMoved.getAsDouble()) > 0.5; // Stop if operator moves drive stick
+    }
 }
